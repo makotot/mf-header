e35229c21abe6ed4798a1de7b5c7a0b38f86c9f3
makotot <m.tateno@prog-8.com>
2021-11-30 17:05:31 +0900

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <header
+      style={{
+        background: '#000',
+        color: "#fff",
+        margin: 0,
+        padding: "4px",
+      }}
+    >
+      <h1>header</h1>
+    </header>
+  );
+};
